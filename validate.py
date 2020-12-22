85448e4aa0fa6210bf4a5e45b914b99fcaa0811f
gvernard <gvernard@astro.rug.nl>
2020-12-22 12:13:50 +0200

    Added scenario runs

--- validate.py
+++ validate.py
@@ -20,9 +20,15 @@ def validate(start_date,end_date,ip_file,output_file):
         print("All good!")
 
 
-
+# Validation 4 days into the future
 validate(start_date="2020-12-12",
          end_date="2020-12-30",
          #ip_file="../../../validation/data/2020-09-30_historical_ip.csv",
          ip_file="data/OxCGRT_latest.csv",
          output_file="predictions/val_4_days.csv")
+
+# Validation 180 into the future
+validate(start_date="2020-10-01",
+         end_date="2021-03-30",
+         ip_file="scenarios/180_days_future_scenario.csv",
+         output_file="predictions/val_6_month_future.csv")
